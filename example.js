75054890ca446c299bb689f3133a64864dfa5991
Bogdan Boiculese <byte1918@gmail.com>
2017-05-02 18:15:24 +0300

    Added more questions and updated default example
    develop

--- example.js
+++ example.js
@@ -1,2 +1,17 @@
 var QuizBot = require('slackquizbot');
-var myQuizBot = new QuizBot('_YOUR_SLACK_BOT_TOKEN_');
+var sleep = require('system-sleep');
+
+var start = function () {
+    try {
+        var bot = new QuizBot('SLACK_TOKEN');
+        setTimeout(function () { bot.startQuiz(null, 'CHANNEL_ID', 'QUIZ_ID'); }, 2000);
+    } catch (ex) {
+        console.log('\nCrashed:' + ex + '\n');
+        console.log('Sleeping for 5 seconds...');
+        sleep(5000);
+        console.log('Trying to reconnect')
+        start();
+    }
+}
+
+start();
